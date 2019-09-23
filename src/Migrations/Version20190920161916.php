07a5d56c381d6d7cea6c9cf1c77f8b70834ea1a1
Joedac <43187778+Joedac@users.noreply.github.com>
2019-09-23 19:35:46 +0200

    update

--- src/Migrations/Version20190920161916.php
+++ src/Migrations/Version20190920161916.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20190919094412 extends AbstractMigration
+final class Version20190920161916 extends AbstractMigration
 {
     public function getDescription() : string
     {
